fc14e81473e8ca85ebb36bc14a77aff644ab1183
MichailoZaichenko <michailo.zaichenko@gmail.com>
2025-11-17 23:59:02 +0200

    from sqlite ot postgres

--- bike_shop_Wellbike/settings.py
+++ bike_shop_Wellbike/settings.py
@@ -1,18 +1,14 @@
 from pathlib import Path
-from dotenv import load_dotenv
 import environ
 import os
 from django.utils.translation import gettext_lazy as _
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
-SECRET_KEY = 'django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 ALLOWED_HOSTS = ['*']
 
@@ -68,12 +64,12 @@ WSGI_APPLICATION = 'bike_shop_Wellbike.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Todo for railway
 # DATABASES = {
@@ -87,12 +83,19 @@ DATABASES = {
 #     }
 # }
 
-# import dj_database_url
 
-# DATABASES = {
-#     'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
 
-# }
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # CACHES = {
