c31f5109574f443bd84c122e6cb1e9ed76df88a4
MichailoZaichenko <michailo.zaichenko@gmail.com>
2025-11-18 00:51:37 +0200

    one more

--- bike_shop_Wellbike/settings.py
+++ bike_shop_Wellbike/settings.py
@@ -143,6 +143,8 @@ CSRF_TRUSTED_ORIGINS = ['https://wellbike.up.railway.app']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+WHITENOISE_MANIFEST_STRICT = False
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfile")
